429cc48f6aada868b3cff2b1d0cd3c686bfaaa48
Benjamin-Rogne <benrog27@gmail.com>
2024-09-11 13:34:29 +0200

    added test 2
    just for experimenting with diffrent commands from lecture

---
+++ TEST/test2.py
@@ -0,0 +1,13 @@
+zoo_animals = ["Panda", "Lion", "Zebra", "Honey Badger", "Aligator"]
+planets = ["Merkur", "Venus", "Jorden", "Jupiter", "Saturn", "Uranus"]
+print(planets[2])
+
+planets[3] = "Mars"
+print(planets)
+
+random_list = ["Europe", 7, ["ny liste", 23, "med nye elementer"], "bil"]
+print(random_list[2][2])
+
+planets.insert(2, "pluto")
+planets.append("en planet")
+print(planets)
