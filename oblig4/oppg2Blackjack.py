3cafec7b83b1be5bd0ae1fbedc896fb36be65ebc
Benjamin-Rogne <benrog27@gmail.com>
2024-11-07 12:43:03 +0100

    working on oblig 4

--- oblig4/oppg2Blackjack.py
+++ oblig4/oppg2Blackjack.py
@@ -1,5 +1,21 @@
 import random
 
+def drawCard(cardList):
+    print
+
+def dealerDrawCard(cardList):
+    print
+
+def wantToPlay():
+    print("do you wish to play again?")
+    print("y - Yes")
+    print("n - No")
+    WantToPlayAns = input("Answer: ")
+    if WantToPlayAns == "no":
+        play = play -1
+        print("See you again next time!:)")
+
+
 play = 1
 while play == 1:
-    print
+    wantToPlay()
